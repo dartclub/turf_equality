260af52c3af9b2c98cbbc54e7e0a6699560152f1
armantorkzaban <armantorkzaban@icloud.com>
2022-06-17 14:07:19 +0200

    porting

--- lib/src/geojson_equality_base.dart
+++ lib/src/geojson_equality_base.dart
@@ -1,3 +1,5 @@
+import 'package:turf/helpers.dart';
+
 class Equality {
 final int precision;
 final bool direction;
@@ -5,193 +7,184 @@ final bool pseudoNode;
  Function? objectComparator;
 
   Equality({this.precision = 17, this.direction = false, this.pseudoNode = false, this.objectComparator}){
-objectComparator = objectComparator ?? (){}; //todo
+objectComparator = objectComparator ??  () {return deepEqual(obj1, obj2, {strict: true});};
+
   }
 
-//   compare(g1,g2) {
-//   if (g1.type !== g2.type || !sameLength(g1,g2)) return false;
+compare (GeoJSONObject g1, GeoJSONObject g2) {
+  if (g1.type != g2.type || !sameLength(g1,g2)) return false;
 
-//   switch(g1.type) {
-//   case 'Point':
-//     return this.compareCoord(g1.coordinates, g2.coordinates);
-//     break;
-//   case 'LineString':
-//     return this.compareLine(g1.coordinates, g2.coordinates,0,false);
-//     break;
-//   case 'Polygon':
-//     return this.comparePolygon(g1,g2);
-//     break;
-//   case 'GeometryCollection':
-//     return this.compareGeometryCollection(g1, g2);
-//   case 'Feature':
-//     return this.compareFeature(g1, g2);
-//   case 'FeatureCollection':
-//     return this.compareFeatureCollection(g1, g2);
-//   default:
-//     if (g1.type.indexOf('Multi') === 0) {
-//       var context = this;
-//       var g1s = explode(g1);
-//       var g2s = explode(g2);
-//       return g1s.every(function(g1part) {
-//         return this.some(function(g2part) {
-//           return context.compare(g1part,g2part);
-//         });
-//       },g2s);
-//     }
-//   }
-//   return false;
-// }
-// }
+  switch(g1.type) {
 
+  case GeoJSONObjectType.lineString:
+    return compareLine((g1 as LineString).coordinates, (g2 as LineString).coordinates,0,false);
+  case GeoJSONObjectType.polygon:
+    return comparePolygon(g1,g2);
+  case GeoJSONObjectType.geometryCollection:
+    return compareGeometryCollection(g1, g2);
+  case GeoJSONObjectType.featureCollection:
+    return compareFeatureCollection(g1, g2);
+  default:
+    if (g1.type == GeoJSONObjectType.multiLineString || g1.type == GeoJSONObjectType.multiPoint || g1.type == GeoJSONObjectType.multiPolygon) {
+      var g1s = explode(g1);
+      var g2s = explode(g2);
+      return g1s.every((g1part) {
+        return this.some((g2part) {
+          return compare(g1part,g2part);
+        });
+      },g2s);
+    }
+  }
+  return false;
+}
 
-// function explode(g) {
-//   return g.coordinates.map(function(part) {
-//     return {
-//       type: g.type.replace('Multi', ''),
-//       coordinates: part}
-//   });
-// }
-// //compare length of coordinates/array
-// function sameLength(g1,g2) {
-//    return g1.hasOwnProperty('coordinates') ?
-//     g1.coordinates.length === g2.coordinates.length
-//     : g1.length === g2.length;
-// }
+explode(g) {
+  return g.coordinates.map((part) {
+    return {
+      type: g.type.replace('Multi', ''),
+      coordinates: part}
+  });
+}
+//compare length of coordinates/array
+sameLength(g1,g2) {
+   return g1.hasOwnProperty('coordinates') ?
+    g1.coordinates.length == g2.coordinates.length
+    : g1.length == g2.length;
+}
 
-// // compare the two coordinates [x,y]
-// Equality.prototype.compareCoord = function(c1,c2) {
-//   if (c1.length !== c2.length) {
-//     return false;
-//   }
+// compare the two coordinates [x,y]
+compareCoord (c1,c2) {
+  if (c1.length != c2.length) {
+    return false;
+  }
 
-//   for (var i=0; i < c1.length; i++) {
-//     if (c1[i].toFixed(this.precision) !== c2[i].toFixed(this.precision)) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+  for (var i=0; i < c1.length; i++) {
+    if (c1[i].toFixed(precision) != c2[i].toFixed(precision)) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// Equality.prototype.compareLine = function(path1,path2,ind,isPoly) {
-//   if (!sameLength(path1,path2)) return false;
-//   var p1 = this.pseudoNode ? path1 : this.removePseudo(path1);
-//   var p2 = this.pseudoNode ? path2 : this.removePseudo(path2);
-//   if (isPoly && !this.compareCoord(p1[0],p2[0])) {
-//     // fix start index of both to same point
-//     p2 = this.fixStartIndex(p2,p1);
-//     if(!p2) return;
-//   }
-//   // for linestring ind =0 and for polygon ind =1
-//   var sameDirection = this.compareCoord(p1[ind],p2[ind]);
-//   if (this.direction || sameDirection
-//   ) {
-//     return this.comparePath(p1, p2);
-//   } else {
-//     if (this.compareCoord(p1[ind],p2[p2.length - (1+ind)])
-//     ) {
-//       return this.comparePath(p1.slice().reverse(), p2);
-//     }
-//     return false;
-//   }
-// };
-// Equality.prototype.fixStartIndex = function(sourcePath,targetPath) {
-//   //make sourcePath first point same as of targetPath
-//   var correctPath,ind = -1;
-//   for (var i=0; i< sourcePath.length; i++) {
-//     if(this.compareCoord(sourcePath[i],targetPath[0])) {
-//       ind = i;
-//       break;
-//     }
-//   }
-//   if (ind >= 0) {
-//     correctPath = [].concat(
-//       sourcePath.slice(ind,sourcePath.length),
-//       sourcePath.slice(1,ind+1));
-//   }
-//   return correctPath;
-// };
-// Equality.prototype.comparePath = function (p1,p2) {
-//   var cont = this;
-//   return p1.every(function(c,i) {
-//     return cont.compareCoord(c,this[i]);
-//   },p2);
-// };
+compareLine (path1,path2,ind,isPoly) {
+  if (!sameLength(path1,path2)) return false;
+  var p1 = pseudoNode ? path1 : removePseudo(path1);
+  var p2 = pseudoNode ? path2 : removePseudo(path2);
+  if (isPoly && !compareCoord(p1[0],p2[0])) {
+    // fix start index of both to same point
+    p2 = fixStartIndex(p2,p1);
+    if(!p2) return;
+  }
+  // for linestring ind =0 and for polygon ind =1
+  var sameDirection = compareCoord(p1[ind],p2[ind]);
+  if (direction || sameDirection
+  ) {
+    return comparePath(p1, p2);
+  } else {
+    if (compareCoord(p1[ind],p2[p2.length - (1+ind)])
+    ) {
+      return comparePath(p1.slice().reverse(), p2);
+    }
+    return false;
+  }
+}
 
-// Equality.prototype.comparePolygon = function(g1,g2) {
-//   if (this.compareLine(g1.coordinates[0],g2.coordinates[0],1,true)) {
-//     var holes1 = g1.coordinates.slice(1,g1.coordinates.length);
-//     var holes2 = g2.coordinates.slice(1,g2.coordinates.length);
-//     var cont = this;
-//     return holes1.every(function(h1) {
-//       return this.some(function(h2) {
-//         return cont.compareLine(h1,h2,1,true);
-//       });
-//     },holes2);
-//   } else {
-//     return false;
-//   }
-// };
+fixStartIndex (sourcePath,targetPath) {
+  //make sourcePath first point same as of targetPath
+  var correctPath,ind = -1;
+  for (var i=0; i< sourcePath.length; i++) {
+    if(compareCoord(sourcePath[i],targetPath[0])) {
+      ind = i;
+      break;
+    }
+  }
+  if (ind >= 0) {
+    correctPath = []..addAll(
+      sourcePath.slice(ind,sourcePath.length),
+      sourcePath.slice(1,ind+1));
+  }
+  return correctPath;
+}
 
-// Equality.prototype.compareGeometryCollection= function(g1,g2) {
-//   if (
-//     !sameLength(g1.geometries, g2.geometries) ||
-//     !this.compareBBox(g1,g2)
-//   ) {
-//     return false;
-//   }
-//   for (var i=0; i < g1.geometries.length; i++) {
-//     if (!this.compare(g1.geometries[i], g2.geometries[i])) {
-//       return false;
-//     }
-//   }
-//   return true
-// };
+comparePath  (p1,p2) {
+  var cont = this;
+  return p1.every((c,i) {
+    return cont.compareCoord(c,this[i]);
+  },p2);
+}
 
-// Equality.prototype.compareFeature = function(g1,g2) {
-//   if (
-//     g1.id !== g2.id ||
-//     !this.objectComparator(g1.properties, g2.properties) ||
-//     !this.compareBBox(g1,g2)
-//   ) {
-//     return false;
-//   }
-//   return this.compare(g1.geometry, g2.geometry);
-// };
+comparePolygon (g1,g2) {
+  if (compareLine(g1.coordinates[0],g2.coordinates[0],1,true)) {
+    var holes1 = g1.coordinates.slice(1,g1.coordinates.length);
+    var holes2 = g2.coordinates.slice(1,g2.coordinates.length);
+    var cont = this;
+    return holes1.every((h1) {
+      return this.some((h2) {
+        return cont.compareLine(h1,h2,1,true);
+      });
+    },holes2);
+  } else {
+    return false;
+  }
+}
 
-// Equality.prototype.compareFeatureCollection = function(g1,g2) {
-//   if (
-//     !sameLength(g1.features, g2.features) ||
-//     !this.compareBBox(g1,g2)
-//   ) {
-//     return false;
-//   }
-//   for (var i=0; i < g1.features.length; i++) {
-//     if (!this.compare(g1.features[i], g2.features[i])) {
-//       return false;
-//     }
-//   }
-//   return true
-// };
+compareGeometryCollection(g1,g2) {
+  if (
+    !sameLength(g1.geometries, g2.geometries) ||
+    !compareBBox(g1,g2)
+  ) {
+    return false;
+  }
+  for (var i=0; i < g1.geometries.length; i++) {
+    if (!compare(g1.geometries[i], g2.geometries[i])) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// Equality.prototype.compareBBox = function(g1,g2) {
-//   if (
-//     (!g1.bbox && !g2.bbox) || 
-//     (
-//       g1.bbox && g2.bbox &&
-//       this.compareCoord(g1.bbox, g2.bbox)
-//     )
-//   )  {
-//     return true;
-//   }
-//   return false;
-// };
-// Equality.prototype.removePseudo = function(path) {
-//   //TODO to be implement
-//   return path;
-// };
+compareFeature (g1,g2) {
+  if (
+    g1.id != g2.id ||
+    !objectComparator(g1.properties, g2.properties) ||
+    !compareBBox(g1,g2)
+  ) {
+    return false;
+  }
+  return compare(g1.geometry, g2.geometry);
+}
 
-// function objectComparator(obj1, obj2) {
-//   return deepEqual(obj1, obj2, {strict: true});
-// }
+compareFeatureCollection (g1,g2) {
+  if (
+    !sameLength(g1.features, g2.features) ||
+    !compareBBox(g1,g2)
+  ) {
+    return false;
+  }
+  for (var i=0; i < g1.features.length; i++) {
+    if (!compare(g1.features[i], g2.features[i])) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// module.exports = Equality;
+compareBBox (g1,g2) {
+  if (
+    (!g1.bbox && !g2.bbox) || 
+    (
+      g1.bbox && g2.bbox &&
+      this.compareCoord(g1.bbox, g2.bbox)
+    )
+  )  {
+    return true;
+  }
+  return false;
+}
+
+removePseudo (path) {
+  //TODO to be implement
+  return path;
+}
+
+}
