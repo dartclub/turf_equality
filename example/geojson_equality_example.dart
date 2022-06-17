ac4ad23905cda56202c074f44c35ec8863c1eb82
armantorkzaban <armantorkzaban@icloud.com>
2022-06-17 13:10:27 +0200

    init

---
+++ example/geojson_equality_example.dart
@@ -0,0 +1,6 @@
+import 'package:geojson_equality/geojson_equality.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/geojson_equality_test.dart
@@ -0,0 +1,16 @@
+import 'package:geojson_equality/geojson_equality.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
